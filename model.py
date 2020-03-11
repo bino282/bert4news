43c07a75a05aeef545427956504475f5d52cc01b
Nhã Nguyễn Vạn <nha282@gmail.com>
2020-03-11 20:13:26 +0700

    update

--- model.py
+++ model.py
@@ -8,18 +8,36 @@ class BertClassification(BertPreTrainedModel):
        super(BertClassification, self).__init__(config)
        self.num_labels = config.num_labels
        self.bert = BertModel(config)
-       self.outputs = nn.Linear(config.hidden_size, self.num_labels)
-
+       self.dropout = nn.Dropout(config.hidden_dropout_prob)
+       self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
        self.init_weights()
 
-   def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
-                start_positions=None, end_positions=None):
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
+                position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
 
-       outputs = self.bert(input_ids,
+        outputs = self.bert(input_ids,
                             attention_mask=attention_mask,
                             token_type_ids=token_type_ids,
                             position_ids=position_ids,
-                            head_mask=head_mask)
-       cls_output = torch.cat((outputs[2][-1][:,0, ...],outputs[2][-2][:,0, ...], outputs[2][-3][:,0, ...], outputs[2][-4][:,0, ...]),-1)
-       logits = self.outputs(cls_output)
-       return logits
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+        #pooled_output = outputs[1]
+
+        pooled_output = torch.cat((outputs[2][-1][:,0, ...],outputs[2][-2][:,0, ...], outputs[2][-3][:,0, ...], outputs[2][-4][:,0, ...]),-1)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
