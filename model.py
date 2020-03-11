98c0a7adfa301f8a2af733e9e308ad28a4cbf499
Nhã Nguyễn Vạn <nha282@gmail.com>
2020-03-11 20:19:55 +0700

    update

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from transformers import *
 
 
 class BertClassification(BertPreTrainedModel):
-   def __init__(self, config):
+    def __init__(self, config):
        super(BertClassification, self).__init__(config)
        self.num_labels = config.num_labels
        self.bert = BertModel(config)
@@ -14,7 +14,6 @@ class BertClassification(BertPreTrainedModel):
 
     def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
                 position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
-
         outputs = self.bert(input_ids,
                             attention_mask=attention_mask,
                             token_type_ids=token_type_ids,
